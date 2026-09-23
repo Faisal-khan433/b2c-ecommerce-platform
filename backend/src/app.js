0256d9bae4635b594e8416da85b97a247c900e00
Faisal-Khan433 <khanfaisal15689@gmail.com>
2026-09-23 16:12:37 +0530

    feat: add error handling and refine cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
 import healthRoutes from "./routes/health.routes.js";
+import notFound from "./middleware/notFound.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+  }));
 
 app.use(express.json());
 
 app.use("/health", healthRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 export default app;
